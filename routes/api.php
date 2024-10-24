a24c8aa6f4d0240cc9b33dcb1ed2151e277de5cf
Talented-Dev934 <alexbenny544@gmail.com>
2024-10-24 21:35:51 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,16 +2,6 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::group(['prefix' => '/'], function () {
     Route::get('get_system_info', 'ApiController@getSystemInfo');
